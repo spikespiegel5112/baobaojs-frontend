5583985e890c47a6a6746ad1eb5e1682d09a552b
spikespiegel <583288277@qq.com>
2026-09-04 06:52:45 +0800

    temp

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -10,6 +10,9 @@ axios.defaults.baseURL =
 // Add a response interceptor
 axios.interceptors.response.use(
   function onFulfilled(response) {
+    console.log(response);
+    // dispatch(setUserInfo(null));
+    // dispatch(setIsLoggedIn(false));
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return Promise.resolve(response.data);
