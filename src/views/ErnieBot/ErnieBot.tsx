0c97ab42136627c9f19ec9102a04d7dbf4117329
spikespiegel <583288277@qq.com>
2026-09-12 08:38:21 +0800

    temp

--- src/views/ErnieBot/ErnieBot.tsx
+++ src/views/ErnieBot/ErnieBot.tsx
@@ -1,11 +1,13 @@
 import "./ErnieBot.scss";
 
 export default function ErnieBot() {
+  const timestamp = new Date().getTime();
+
   return (
     <div className="erniebot_container">
       <iframe
         title="ErnieBot"
-        src="https://baobaojs.com/chat"
+        src={`https://baobaojs.com/chat?iframe=${timestamp}`}
       ></iframe>
     </div>
   );
